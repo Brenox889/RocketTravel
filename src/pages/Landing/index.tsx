8d1055c85b6efb57a30ac99f32725371ce343592
Brenox889 <brenomendes889@hotmail.com>
2020-09-02 23:26:03 -0300

    feat: Landing struture

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ import { Container } from './styles';
+
+const Landing: React.FC = () => {
+  return(
+
+    <Logo />
+    <Banner>
+    <Button>Ver Mais</Button>
+    </Banner>
+    <Card /> 
+    <Card /> 
+  );
+}
+
+export default Landing;
